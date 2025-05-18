3f6f8c01d3d59f53b2d6dd864413c542f26fa7a5
Limon00001 <limonhossain82@gmail.com>
2025-05-18 22:17:50 +0600

    feat(user-info): implement profile options

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -5,10 +5,14 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+import { useState } from 'react';
+
 // UserInfo Component
 const UserInfo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between items-center p-5">
+    <div className="relative flex justify-between items-center p-5">
       <div className="flex items-center gap-5">
         <img
           src="./avatar.png"
@@ -18,10 +22,24 @@ const UserInfo = () => {
         <h2>John Doe</h2>
       </div>
       <div className="flex gap-5">
-        <img src="./more.png" alt="" className="h-5 w-5 cursor-pointer" />
+        <img
+          src="./more.png"
+          alt=""
+          className="h-5 w-5 cursor-pointer"
+          onClick={() => setIsOpen((prev) => !prev)}
+        />
         <img src="./video.png" alt="" className="h-5 w-5 cursor-pointer" />
         <img src="./edit.png" alt="" className="h-5 w-5 cursor-pointer" />
       </div>
+      {isOpen && (
+        <div className="absolute top-16 right-2 w-30 p-2 bg-teal-600/50 backdrop-blur-md rounded-lg shadow-lg z-50">
+          <p className="cursor-pointer text-sm font-normal tracking-wider">
+            Edit Profile
+          </p>
+          <hr className="my-2 text-gray-400" />
+          <p className="cursor-pointer text-sm font-normal">Logout</p>
+        </div>
+      )}
     </div>
   );
 };
