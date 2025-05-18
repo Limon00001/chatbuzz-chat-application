b78d8b26a315b824ad823d0f79dab3aecb0c7a83
Limon00001 <limonhossain82@gmail.com>
2025-05-18 15:44:04 +0600

    feat(connectedUsers): implement ConnectedUsers component for displaying online & offline users

---
+++ src/components/list/chatList/connectedUsers/ConnectedUsers.jsx
@@ -0,0 +1,61 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 18 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// ConnectedUsers Component
+const ConnectedUsers = () => {
+  return (
+    <>
+      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700">
+        <img
+          src="./avatar.png"
+          alt=""
+          className="h-13 w-13 rounded-full object-cover"
+        />
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">John Doe</span>
+          <p className="text-sm">message</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700">
+        <img
+          src="./avatar.png"
+          alt=""
+          className="h-13 w-13 rounded-full object-cover"
+        />
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">John Doe</span>
+          <p className="text-sm">message</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700">
+        <img
+          src="./avatar.png"
+          alt=""
+          className="h-13 w-13 rounded-full object-cover"
+        />
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">John Doe</span>
+          <p className="text-sm">message</p>
+        </div>
+      </div>
+      <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700">
+        <img
+          src="./avatar.png"
+          alt=""
+          className="h-13 w-13 rounded-full object-cover"
+        />
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">John Doe</span>
+          <p className="text-sm">message</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// Export
+export default ConnectedUsers;
