bf522a51fbe51f7d470379540fc040b1324b2b3b
Limon00001 <limonhossain82@gmail.com>
2025-05-21 21:54:14 +0600

    feat(SearchToolbar): enhance search functionality

--- src/components/list/chatList/searchToolbar/SearchToolbar.jsx
+++ src/components/list/chatList/searchToolbar/SearchToolbar.jsx
@@ -5,24 +5,44 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import debounce from 'lodash/debounce';
+import { Minus, Plus, Search } from 'lucide-react';
+import { useCallback } from 'react';
+
 // SearchToolbar Component
-const SearchToolbar = ({ addMode, onAddMode }) => {
+const SearchToolbar = ({ addMode, onAddMode, searchTerm, setSearchTerm }) => {
+  const debouncedSearch = useCallback(
+    debounce((value) => {
+      setSearchTerm(value);
+    }, 300),
+    [setSearchTerm],
+  );
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    e.persist();
+    debouncedSearch(value);
+  };
   return (
     <div className="flex items-center p-5 gap-5">
       <div className="flex-1 flex items-center gap-5 bg-black/50 rounded-xl p-2">
-        <img src="./search.png" alt="" className="h-5 w-5" />
+        <Search className="h-5 w-5 text-gray-400" />
         <input
           type="text"
           className="flex-1 bg-transparent border-none outline-none text-white"
           placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearch}
         />
       </div>
-      <img
-        src={addMode ? './minus.png' : './plus.png'}
-        alt="add user"
-        className="h-9 w-9 cursor-pointer bg-black/50 p-[10px] rounded-xl"
-        onClick={() => onAddMode((prev) => !prev)}
-      />
+      <button onClick={() => onAddMode((prev) => !prev)}>
+        {addMode ? (
+          <Minus className="h-8 w-8 text-gray-400 cursor-pointer bg-black/50 p-1 rounded-xl" />
+        ) : (
+          <Plus className="h-8 w-8 text-gray-400 cursor-pointer bg-black/50 p-1 rounded-xl" />
+        )}
+      </button>
     </div>
   );
 };
