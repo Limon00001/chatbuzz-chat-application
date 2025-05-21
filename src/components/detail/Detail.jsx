da5d75146ceaa5e6405380e691cee9bb78fae0b8
Limon00001 <limonhossain82@gmail.com>
2025-05-21 21:52:49 +0600

    feat(Detail): implement user blocking functionality and enhance user display

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -5,73 +5,62 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+// Internal Imports
+import { useChatStore } from '../../lib/chatStore';
+import { db } from '../../lib/firebase';
+import { useUserStore } from '../../lib/userStore';
+
 // Detail Component
 const Detail = () => {
+  const { currentUser } = useUserStore();
+  const { user, isCurrentUserBlocked, isRecieverBlocked, changeBlock } =
+    useChatStore();
+
+  const handleBlock = async () => {
+    if (!user) return;
+
+    const userDocRef = doc(db, 'users', currentUser.id);
+
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isRecieverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+
+      changeBlock();
+    } catch (error) {
+      toast.error(error.message || 'Something went wrong');
+    }
+  };
+
   return (
-    <section className="flex-1">
+    <section className="flex-1 flex flex-col h-full">
       <div className="flex flex-col items-center gap-5 p-[30px_20px] border-b border-b-gray-700">
-        <img src="./avatar.png" alt="" className="w-25 h-25 rounded-full" />
-        <h2>John Doe</h2>
+        <img
+          src={user?.profileImage || './profile.png'}
+          alt=""
+          className="w-25 h-25 rounded-full"
+        />
+        <h2>{user?.username}</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
-      <div className="flex flex-col gap-8 p-5">
-        <div>
-          <div className="flex items-center justify-between">
-            <span>Chat Settings</span>
-            <img
-              src="./arrowUp.png"
-              alt=""
-              className="h-7 w-7 p-2 bg-black/30 rounded-full cursor-pointer"
-            />
-          </div>
-        </div>
-        <div>
-          <div className="flex items-center justify-between">
-            <span>Privacy Settings</span>
-            <img
-              src="./arrowUp.png"
-              alt=""
-              className="h-7 w-7 p-2 bg-black/30 rounded-full cursor-pointer"
-            />
-          </div>
-        </div>
-        <div>
-          <div className="flex items-center justify-between">
-            <span>Shared Photos</span>
-            <img
-              src="./arrowDown.png"
-              alt=""
-              className="h-7 w-7 p-2 bg-black/30 rounded-full cursor-pointer"
-            />
-          </div>
-          <div className="flex flex-col gap-5 mt-5">
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-5">
-                <img
-                  src="./avatar.png"
-                  alt=""
-                  className="w-10 h-10 rounded-md object-cover"
-                />
-                <span className="text-sm text-gray-200 font-normal">
-                  Photo Name
-                </span>
-              </div>
-              <img
-                src="./download.png"
-                alt=""
-                className="h-7 w-7 p-[6px] cursor-pointer"
-              />
-            </div>
-          </div>
-        </div>
-        <div>
-          <div className="flex items-center justify-between">
-            <span>Shared Files</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <button className="w-full bg-red-600/80 hover:bg-red-600/60 py-2 px-4 border-none rounded-full transition-colors duration-300 ease-in-out cursor-pointer">
-          Block User
+      <div className="flex flex-col gap-8 p-5 mt-auto">
+        <button
+          onClick={handleBlock}
+          className={`w-full bg-red-600/80 hover:bg-red-600/60 py-2 px-4 border-none rounded-full transition-colors duration-300 ease-in-out cursor-pointer ${
+            isCurrentUserBlocked
+              ? 'opacity-50 cursor-not-allowed pointer-events-none'
+              : ''
+          }`}
+        >
+          {isCurrentUserBlocked
+            ? 'You are Blocked!'
+            : isRecieverBlocked
+            ? 'User is Blocked!'
+            : 'Block User'}
         </button>
       </div>
     </section>
