83a85712a9b922130650f0a8dbf10fb28fe3ae6f
Limon00001 <limonhossain82@gmail.com>
2025-05-21 21:56:50 +0600

    feat(UserInfo): refactor component

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -6,7 +6,7 @@
  */
 
 // External Imports
-import { useEffect, useRef, useState } from 'react';
+import { LogOut, SquarePen } from 'lucide-react';
 
 // Internal Imports
 import { auth } from '../../../lib/firebase';
@@ -14,21 +14,7 @@ import { useUserStore } from '../../../lib/userStore';
 
 // UserInfo Component
 const UserInfo = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const popupRef = useRef(null);
-
   const { currentUser } = useUserStore();
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (popupRef.current && !popupRef.current?.contains(event.target)) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, []);
 
   return (
     <div className="relative flex justify-between items-center p-5">
@@ -40,33 +26,17 @@ const UserInfo = () => {
         />
         <h2>{currentUser?.username}</h2>
       </div>
-      <div className="flex gap-5">
-        <img
-          src="./more.png"
-          alt=""
-          className="h-5 w-5 cursor-pointer"
-          onClick={() => setIsOpen((prev) => !prev)}
-        />
-        <img src="./video.png" alt="" className="h-5 w-5 cursor-pointer" />
-        <img src="./edit.png" alt="" className="h-5 w-5 cursor-pointer" />
-      </div>
-      {isOpen && (
-        <div
-          ref={popupRef}
-          className="absolute top-16 right-2 w-30 p-2 bg-teal-600/50 backdrop-blur-md rounded-lg shadow-lg z-50"
+      <div className="flex items-center gap-2">
+        <button className="cursor-pointer bg-black/30 p-2 rounded-full">
+          <SquarePen className="w-5 h-5" />
+        </button>
+        <button
+          onClick={() => auth.signOut()}
+          className="cursor-pointer text-red-500/70 bg-black/30 p-2 rounded-full"
         >
-          <button className="cursor-pointer text-sm font-normal tracking-wider">
-            Edit Profile
-          </button>
-          <hr className="my-2 text-gray-400" />
-          <button
-            onClick={() => auth.signOut()}
-            className="cursor-pointer text-sm font-normal"
-          >
-            Logout
-          </button>
-        </div>
-      )}
+          <LogOut className="w-5 h-5" />
+        </button>
+      </div>
     </div>
   );
 };
