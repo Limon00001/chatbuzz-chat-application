c9770f70bd27621df192784edea79501d14acb16
Limon00001 <limonhossain82@gmail.com>
2025-05-21 14:30:59 +0600

    feat(ConnectedUsers): add chat selection handling and update status

--- src/components/list/chatList/connectedUsers/ConnectedUsers.jsx
+++ src/components/list/chatList/connectedUsers/ConnectedUsers.jsx
@@ -6,10 +6,11 @@
  */
 
 // External Imports
-import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
 // Internal Imports
+import { useChatStore } from '../../../../lib/chatStore';
 import { db } from '../../../../lib/firebase';
 import { useUserStore } from '../../../../lib/userStore';
 
@@ -18,6 +19,7 @@ const ConnectedUsers = () => {
   const [chats, setChats] = useState([]);
 
   const { currentUser } = useUserStore();
+  const { changeChat } = useChatStore();
 
   useEffect(() => {
     const unSub = onSnapshot(
@@ -47,19 +49,49 @@ const ConnectedUsers = () => {
     };
   }, [currentUser.id]);
 
+  const handleSelect = async (chat) => {
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId,
+    );
+
+    userChats[chatIndex].isSeen = true;
+    const userChatsRef = await doc(db, 'userchats', currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+
+      changeChat(chat.chatId, chat.user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      {chats.map((chat) => (
-        <div className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700">
+      {chats?.map((chat) => (
+        <div
+          key={chat.chatId}
+          className={`flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-700 ${
+            chat?.isSeen ? 'bg-transparent' : 'bg-teal-500/30'
+          }`}
+          onClick={() => handleSelect(chat)}
+        >
           <img
-            src={chat.user.profileImage || './profile.png'}
+            src={chat?.user?.profileImage || './profile.png'}
             alt=""
             className="h-12 w-12 rounded-full object-cover"
           />
 
           <div key={chat.chatId} className="flex flex-col gap-2">
-            <span className="font-semibold">{chat.user.username}</span>
-            <p className="text-sm text-gray-400">{chat.lastMessage}</p>
+            <span className="font-semibold">{chat?.user?.username}</span>
+            <p className="text-sm text-gray-400">{chat?.lastMessage}</p>
           </div>
         </div>
       ))}
---
+++ src/lib/chatStore.js
@@ -0,0 +1,55 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 21 May, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import { create } from 'zustand';
+
+// Internal Imports
+import { useUserStore } from './userStore';
+
+// Internal Imports
+
+// User Store
+const useChatStore = create((set) => ({
+  chatId: null,
+  user: null,
+  isCurrentUserBlocked: false,
+  isRecieverBlocked: false,
+  changeChat: (chatId, user) => {
+    const currentUser = useUserStore.getState().currentUser;
+
+    if (user.blocked.includes(currentUser.id)) {
+      return set({
+        chatId,
+        user: null,
+        isCurrentUserBlocked: true,
+        isRecieverBlocked: false,
+      });
+    } else if (currentUser.blocked.includes(user.id)) {
+      return set({
+        chatId,
+        user,
+        isCurrentUserBlocked: false,
+        isRecieverBlocked: true,
+      });
+    } else {
+      return set({
+        chatId,
+        user,
+        isCurrentUserBlocked: false,
+        isRecieverBlocked: false,
+      });
+    }
+  },
+
+  changeBlock: () => {
+    set((state) => ({ ...state, isRecieverBlocked: !state.isRecieverBlocked }));
+  },
+}));
+
+// Export
+export { useChatStore };
